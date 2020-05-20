1189729882eb7b9728798a3353ab028dc69d4a5d
irek.m <irek.m@kurs.pl>
2020-05-20 20:45:22 +0200

    New functions to convert to ASCII with optimized code

--- src/main/java/AsciiConverterApp.java
+++ src/main/java/AsciiConverterApp.java
@@ -6,19 +6,21 @@ public class AsciiConverterApp {
     public static void main(String[] args) {
         String testedText = "gstmarszi";
 
-        System.out.println("ASCII coding: String provided via Interface and printed  with | separator.");
-        asciiCodePrint();
+        /*System.out.println("ASCII coding: String provided via Interface and printed  with | separator.");
+        printAscii();
 
         System.out.println("\n\nASCII coding: String provided as method parameters and printed with | separator.");
         System.out.println("Text provided: " + testedText);
-        asciiCodePrint(testedText);
+        printAscii(testedText);
 
         System.out.println("\n\nASCII coding: String provided via Interface and returned as String with | separator.");
-        System.out.print(asciiCodeReturn());
+        System.out.print(fetchAscii());
 
         System.out.println("\n\nASCII coding: String provided as method parameters and returned as String with | separator.");
         System.out.println("Text provided: " + testedText);
-        System.out.print(asciiCodeReturn(testedText));
+        System.out.print(fetchAscii(testedText));*/
+
+        printFromAscii("123!");
 
     }
 }
--- src/main/java/stringTools/AsciiConverter.java
+++ src/main/java/stringTools/AsciiConverter.java
@@ -1,63 +1,48 @@
 package stringTools;
 
+import javax.xml.crypto.dsig.spec.XPathType;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static interfaceProviders.ParametersProvider.*;
 
 public class AsciiConverter {
     static final char SEPARATOR = '|';
 
+    public static void printFromAscii(String ascii){
+        if(validateInputString(ascii)){
+
+        }else
+            System.out.println("Incorrect input string");
+    }
+
     //
     //    Converts String delivered via Interface
     //    Prints result with SEPARATOR
     //
-    public static void asciiCodePrint (){
+    public static void printAscii (){
         String text = provideString("Enter the string to be converted: ");
         char[] letterFromTextTable = text.toCharArray();
-        int index = 1;
-        // TODO: think how to get rid of loop, put it into method.
-        for ( char letter: letterFromTextTable ) {
-            if (index != letterFromTextTable.length) {
-                letterAsciiCodePrint(letter, false);
-                index++;
-            }else{
-                letterAsciiCodePrint(letter, true);
-            }
-        }
+        printAsciiFromCharArgs(letterFromTextTable);
     }
 
     //
     //    Converts String delivered via method parameter
     //    Prints result with SEPARATOR
     //
-    public static void asciiCodePrint (String text){
+    public static void printAscii (String text){
         char[] letterFromTextTable = text.toCharArray();
-        int index = 1;
-        for ( char letter: letterFromTextTable ) {
-            if (index != letterFromTextTable.length) {
-                letterAsciiCodePrint(letter, false);
-                index++;
-            }else{
-                letterAsciiCodePrint(letter, true);
-            }
-        }
+        printAsciiFromCharArgs(letterFromTextTable);
     }
 
     //
     //    Converts String delivered via Interface
     //    Returns result as String with SEPARATOR
     //
-    public static String asciiCodeReturn (){
+    public static String fetchAscii (){
         String text = provideString("Enter the string to be converted: ");
         char[] letterFromTextTable = text.toCharArray();
-        StringBuilder result = new StringBuilder();
-        int index = 1;
-        for ( char letter: letterFromTextTable ) {
-            if (index != letterFromTextTable.length) {
-                result.append(letterAsciiCodeReturn(letter, false));
-                index++;
-            }else{
-                result.append(letterAsciiCodeReturn(letter, true));
-            }
-        }
+        StringBuilder result = getAsciiFromCharArgs(letterFromTextTable);
         return result.toString();
     }
 
@@ -65,18 +50,9 @@ public class AsciiConverter {
     //    Converts String delivered via Interface
     //    Returns result as String with SEPARATOR
     //
-    public static String asciiCodeReturn (String text){
+    public static String fetchAscii (String text){
         char[] letterFromTextTable = text.toCharArray();
-        StringBuilder result = new StringBuilder();
-        int index = 1;
-        for ( char letter: letterFromTextTable ) {
-            if (index != letterFromTextTable.length) {
-                result.append(letterAsciiCodeReturn(letter, false));
-                index++;
-            }else{
-                result.append(letterAsciiCodeReturn(letter, true));
-            }
-        }
+        StringBuilder result = getAsciiFromCharArgs(letterFromTextTable);
         return result.toString();
     }
 
@@ -84,7 +60,7 @@ public class AsciiConverter {
     //    Converts char to ASCII
     //    Prints result with SEPARATOR
     //
-    public static void letterAsciiCodePrint (char letter, boolean lastLetter){
+    private static void printLetterAscii (char letter, boolean lastLetter){
         if (!lastLetter) {
             System.out.print((int) letter);
             System.out.print(SEPARATOR);
@@ -97,7 +73,7 @@ public class AsciiConverter {
     //    Converts char to ASCII
     //    Returns result with SEPARATOR
     //
-    public static String letterAsciiCodeReturn (char letter, boolean lastLetter){
+    private static String fetchLetterAscii (char letter, boolean lastLetter){
         StringBuilder result = new StringBuilder();
         if (!lastLetter) {
             result.append((int) letter);
@@ -106,5 +82,62 @@ public class AsciiConverter {
             result.append((int) letter);
         }
         return result.toString();
+    }
+
+    //
+    //    Prints ASCII from char Args
+    //    Returns result with SEPARATOR
+    //
+    private static void printAsciiFromCharArgs(char... letterFromTextTable) {
+        int index = 1;
+        for ( char letter: letterFromTextTable ) {
+            if (index != letterFromTextTable.length) {
+                printLetterAscii(letter, false);
+                index++;
+            }else{
+                printLetterAscii(letter, true);
+            }
+        }
+    }
+
+    //
+    //    Returns ASCII from char Args
+    //    Returns result with SEPARATOR
+    //
+    private static StringBuilder getAsciiFromCharArgs(char... letterFromTextTable) {
+        StringBuilder result = new StringBuilder();
+        int index = 1;
+        for (char letter : letterFromTextTable) {
+            if (index != letterFromTextTable.length) {
+                result.append(fetchLetterAscii(letter, false));
+                index++;
+            } else {
+                result.append(fetchLetterAscii(letter, true));
+            }
+        }
+        return result;
+    }
+
+    private static boolean validateInputString (String toValidate){
+        char[] charTbl = toValidate.toCharArray();
+        boolean result = true;
+
+        if (!Character.isDigit(toValidate.charAt(0))){
+            result = false;
+        }
+
+        if (!Character.isDigit(toValidate.charAt(toValidate.length()-1))){
+            result = false;
+        }
+
+        if(result) {
+            for (char sign : charTbl) {
+                if (!(Character.isDigit(sign) || sign == '|')) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }

916319337197e70f587ff96bacdee17412eeda6b
irek.m <irek.m@kurs.pl>
2020-06-01 21:26:08 +0200

    Class Math added with abs. gitignore modified by *.class

---
+++ src/main/java/MathApp.java
@@ -0,0 +1,9 @@
+public class MathApp {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = -1;
+
+        System.out.println(mathTools.Math.abs(x));
+        System.out.println(mathTools.Math.abs(y));
+    }
+}
---
+++ src/main/java/mathTools/Math.java
@@ -0,0 +1,7 @@
+package mathTools;
+
+public class Math {
+    public static int abs(int x){
+        return (x>=0)?x:(-1*x);
+    }
+}

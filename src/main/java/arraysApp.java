9ff2ade07ca83019bccb05d6fc2a34ccf57ac34b
irek.m <irek.m@kurs.pl>
2020-06-02 21:27:50 +0200

    Arrays.itemAt Arrays.itemAtWithSentry Arrays.max Arrays.min

--- src/main/java/arraysApp.java
+++ src/main/java/arraysApp.java
@@ -1,13 +1,42 @@
 public class arraysApp {
     public static void main(String[] args) {
-        float[] floatTbl = {1, 2,3,4,5};
+        float[] floatTbl = {2,3,4,5};
         double[] doubleTbl = {1.1, 2.2,3.3,4.4,5.5};
-        String[] stringTbl = {"1","2","3","4"};
-        char[] charTbl = {'1','2','3','4'};
+        String[] stringTbl = {"1","2","3","4","1"};
+        char[] charTbl = {'1','2','3','4','1'};
 
-        System.out.println(arraysTools.Arrays.sumOfArrayItems(floatTbl));
-        System.out.println(arraysTools.Arrays.sumOfArrayItems(doubleTbl));
-        System.out.println(arraysTools.Arrays.sumOfArrayItems(stringTbl));
-        System.out.println(arraysTools.Arrays.sumOfArrayItems(charTbl));
+        System.out.println("sumOfArrayItems");
+        System.out.println(arraysTools.Arrays.sumOfAllItems(floatTbl));
+        System.out.println(arraysTools.Arrays.sumOfAllItems(doubleTbl));
+        System.out.println(arraysTools.Arrays.sumOfAllItems(stringTbl));
+        System.out.println(arraysTools.Arrays.sumOfAllItems(charTbl));
+
+        System.out.println("countItemFromArray");
+        System.out.println(arraysTools.Arrays.countItemAppearances(1,floatTbl));
+        System.out.println(arraysTools.Arrays.countItemAppearances(1.1,doubleTbl));
+        System.out.println(arraysTools.Arrays.countItemAppearances("1",stringTbl));
+        System.out.println(arraysTools.Arrays.countItemAppearances('1',charTbl));
+
+        System.out.println("min");
+        System.out.println(arraysTools.Arrays.min(floatTbl));
+        System.out.println(arraysTools.Arrays.min(doubleTbl));
+
+        System.out.println("max");
+        System.out.println(arraysTools.Arrays.max(floatTbl));
+        System.out.println(arraysTools.Arrays.max(doubleTbl));
+
+        System.out.println("item at");
+        System.out.println(arraysTools.Arrays.itemAt(4, floatTbl));
+        System.out.println(arraysTools.Arrays.itemAt(4.4, doubleTbl));
+        System.out.println(arraysTools.Arrays.itemAt("4", stringTbl));
+        System.out.println(arraysTools.Arrays.itemAt('4', stringTbl));
+        System.out.println(arraysTools.Arrays.itemAt('4', charTbl));
+
+        System.out.println("item at WithSentry");
+        System.out.println(arraysTools.Arrays.itemAtWithSentry(4, floatTbl));
+        System.out.println(arraysTools.Arrays.itemAtWithSentry(4.4, doubleTbl));
+        System.out.println(arraysTools.Arrays.itemAtWithSentry("4", stringTbl));
+        System.out.println(arraysTools.Arrays.itemAtWithSentry('1', stringTbl));
+        System.out.println(arraysTools.Arrays.itemAtWithSentry('a', charTbl));
     }
 }
--- src/main/java/arraysTools/Arrays.java
+++ src/main/java/arraysTools/Arrays.java
@@ -1,35 +1,298 @@
 package arraysTools;
 
 public class Arrays {
-        public static float sumOfArrayItems(float... arrays){
+        public static float sumOfAllItems(float... array){
             float result=0;
-            for (float a:arrays) {
+            for (float a:array) {
                 result+=a;
             }
             return result;
         }
 
-    public static double sumOfArrayItems(double... arrays){
+    public static double sumOfAllItems(double... array){
         float result=0;
-        for (double a:arrays) {
+        for (double a:array) {
             result+=a;
         }
         return result;
     }
 
-    public static String sumOfArrayItems(String... arrays){
+    public static String sumOfAllItems(String... array){
         StringBuilder result = new StringBuilder();
-        for (String a:arrays) {
+        for (String a:array) {
             result.append(a);
         }
         return result.toString();
     }
 
-    public static String sumOfArrayItems(char... arrays){
+    public static String sumOfAllItems(char... array){
         StringBuilder result = new StringBuilder();
-        for (char a:arrays) {
+        for (char a:array) {
             result.append(a);
         }
         return result.toString();
+    }
+
+    public static int countItemAppearances(float item, float... array){
+            int result = 0;
+        for (float a:array) {
+            if (a == item){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static int countItemAppearances(double item, double... array){
+        int result = 0;
+        for (double a:array) {
+            if (a == item){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static int countItemAppearances(String item, String... array){
+        int result = 0;
+        for (String a:array) {
+            if (a.equals(item)){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static int countItemAppearances(char item, char... array){
+        int result = 0;
+        for (char a:array) {
+            if (a == item){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static float min(float... array){
+        float min = array[0];
+        if (array.length > 1) {
+            for (int i = 1; i < array.length; i++) {
+                if(array[i] < min){
+                    min = array[i];
+                }
+            }
+        }
+        return min;
+    }
+
+    public static double min(double... array){
+        double min = array[0];
+        if (array.length > 1) {
+            for (int i = 1; i < array.length; i++) {
+                if(array[i] < min){
+                    min = array[i];
+                }
+            }
+        }
+        return min;
+    }
+
+    public static float max(float... array){
+        float max = array[0];
+        if (array.length > 1) {
+            for (int i = 1; i < array.length; i++) {
+                if(array[i] > max){
+                    max = array[i];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static double max(double... array){
+        double max = array[0];
+        if (array.length > 1) {
+            for (int i = 1; i < array.length; i++) {
+                if(array[i] > max){
+                    max = array[i];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int itemAt(float item, float... array){
+        int result = -1;
+        int index = 0;
+        for (float a:array) {
+            if(a == item){
+                result = index;
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
+
+    public static int itemAt(double item, double... array){
+        int result = -1;
+        int index = 0;
+        for (double a:array) {
+            if(a == item){
+                result = index;
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
+
+    public static int itemAt(String item, String... array){
+        int result = -1;
+        int index = 0;
+        for (String a:array) {
+            if(a.equals(item)){
+                result = index;
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
+
+    public static int itemAt(char item, String... array){
+        int result = -1;
+        int index = 0;
+        for (String a:array) {
+            if(a.equals(Character.toString(item))){
+                result = index;
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
+
+    public static int itemAt(char item, char... array){
+        int result = -1;
+        int index = 0;
+        for (char a:array) {
+            if(a == item){
+                result = index;
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
+
+    public static int itemAtWithSentry(float item, float... array){
+        int arraySize = array.length;
+        int result = -1;
+        int index = 0;
+
+        float[] tempTbl = java.util.Arrays.copyOf(array, arraySize+1);
+        tempTbl[array.length] = item;
+
+        while(true){
+            if(tempTbl[index] == item){
+                break;
+            }
+            index++;
+        }
+
+        if(index != array.length){
+            result = index;
+        }
+
+        return result;
+    }
+
+    public static int itemAtWithSentry(double item, double... array){
+        int arraySize = array.length;
+        int result = -1;
+        int index = 0;
+
+        double[] tempTbl = java.util.Arrays.copyOf(array, arraySize+1);
+        tempTbl[array.length] = item;
+
+        while(true){
+            if(tempTbl[index] == item){
+                break;
+            }
+            index++;
+        }
+
+        if(index != array.length){
+            result = index;
+        }
+
+        return result;
+    }
+
+    public static int itemAtWithSentry(String item, String... array){
+        int arraySize = array.length;
+        int result = -1;
+        int index = 0;
+
+        String[] tempTbl = java.util.Arrays.copyOf(array, arraySize+1);
+        tempTbl[array.length] = item;
+
+        while(true){
+            if(tempTbl[index].equals(item)){
+                break;
+            }
+            index++;
+        }
+
+        if(index != array.length){
+            result = index;
+        }
+
+        return result;
+    }
+
+    public static int itemAtWithSentry(char item, String... array){
+        int arraySize = array.length;
+        int result = -1;
+        int index = 0;
+
+        String[] tempTbl = java.util.Arrays.copyOf(array, arraySize+1);
+        tempTbl[array.length] = Character.toString(item);
+
+        while(true){
+            if(tempTbl[index].equals(Character.toString(item))){
+                break;
+            }
+            index++;
+        }
+
+        if(index != array.length){
+            result = index;
+        }
+
+        return result;
+    }
+
+    public static int itemAtWithSentry(char item, char... array){
+        int arraySize = array.length;
+        int result = -1;
+        int index = 0;
+
+        char[] tempTbl = java.util.Arrays.copyOf(array, arraySize+1);
+        tempTbl[array.length] = item;
+
+        while(true){
+            if(tempTbl[index] == item){
+                break;
+            }
+            index++;
+        }
+
+        if(index != array.length){
+            result = index;
+        }
+
+        return result;
     }
 }

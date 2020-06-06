347e9b049e739a589918b64e8006cb85e9862016
irek.m <irek.m@kurs.pl>
2020-06-06 22:41:09 +0200

    Math.dominantAnalysis Math.firstDominant

--- src/main/java/MathApp.java
+++ src/main/java/MathApp.java
@@ -1,15 +1,22 @@
+import java.util.Arrays;
+
 public class MathApp {
     public static void main(String[] args) {
         int x = 0;
         int y = -1;
-        int[] intArray = {2,2,2,2,2,2,2,2,2,2};
+        int[] intArray = {2,3,4,4,8,8,8,8,2,1};
 
-        System.out.println(mathTools.Math.abs(x));
+        /*System.out.println(mathTools.Math.abs(x));
         System.out.println(mathTools.Math.abs(y));
 
         System.out.println(mathTools.Math.sumOfDigitsFromInteger(10));
         System.out.println(mathTools.Math.power(10,2));
         System.out.println(mathTools.Math.arithmeticAverage(intArray));
-        System.out.println(mathTools.Math.isEven(1));
+        System.out.println(mathTools.Math.isEven(1));*/
+
+        System.out.println(Arrays.toString(mathTools.Math.dominantAnalysis(intArray)[0]));
+        System.out.println(Arrays.toString(mathTools.Math.dominantAnalysis(intArray)[1]));
+        System.out.println(mathTools.Math.max(intArray));
+        System.out.println(mathTools.Math.firstDominant(intArray));
     }
 }
--- src/main/java/mathTools/Math.java
+++ src/main/java/mathTools/Math.java
@@ -1,5 +1,7 @@
 package mathTools;
 
+import arraysTools.Arrays;
+
 public class Math {
     public static int abs(int x){
         return (x>=0)?x:(-1*x);
@@ -44,4 +46,101 @@ public class Math {
     public static boolean isEven(int number){
         return number % 2 == 0;
     }
+
+    public static int max(int... array){
+        int max = array[0];
+        if (array.length == 0){
+            return 0;
+        }
+
+        if (array.length == 1){
+            return max;
+        }
+
+        for (int a:array) {
+            if (a > max){
+                max = a;
+            }
+        }
+
+        return max;
+    }
+
+    public static int firstDominant(int... array){
+        int [][] tempTbl = dominantAnalysis(array);
+
+        int max = max(tempTbl[1]);
+        int index = 0;
+
+        for (int a:tempTbl[1]) {
+            if (a == max){
+                break;
+            }
+            index++;
+        }
+
+        return tempTbl[0][index] ;
+    }
+
+    public static int[][] dominantAnalysis(int... array){
+        int [][] tempTbl = new int[2][array.length];
+        int newIndex;
+
+        for (int a:array) {
+            if(!ifExistsInIntArray(a, tempTbl[0])){
+                newIndex = findFirstEmptyIndexFromIntArray(tempTbl[1]);
+                tempTbl[0][newIndex] = a;
+                tempTbl[1][newIndex] = 1;
+            }else{
+                newIndex = findIndexOfItemFromIntArray(a,tempTbl[0]);
+                tempTbl[1][newIndex]++;
+            }
+        }
+
+        return tempTbl;
+    }
+
+    private static boolean ifExistsInIntArray (int number, int... array){
+        if (array.length == 0){
+            return false;
+        }
+
+        for (int a:array) {
+            if (number == a){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static int findFirstEmptyIndexFromIntArray (int... array){
+        int result = 0;
+        int index = 0;
+        for (int a:array) {
+            if(a == 0 ){
+                result = index;
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
+
+    private static int findIndexOfItemFromIntArray(int number, int... array){
+        if(!ifExistsInIntArray(number, array)){
+            return -1;
+        }
+
+        int index = 0;
+        for (int a:array) {
+            if(a == number){
+                break;
+            }
+            index++;
+        }
+
+        return index;
+    }
+
 }
